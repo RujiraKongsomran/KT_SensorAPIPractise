b8d75648579c1bc3eded38186cb6519e1ea07700
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-09-26 10:20:31 +0700

    Get a listing of every sensor

--- app/src/main/java/com/rujirakongsomran/kt_sensorapipractise/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_sensorapipractise/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.rujirakongsomran.kt_sensorapipractise
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sensorManager: SensorManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        // get a listing of every sensor on a device by calling the getSensorList()
+        val deviceSensors: List<Sensor> =
+            sensorManager.getSensorList(Sensor.TYPE_ALL)
+
     }
 }
